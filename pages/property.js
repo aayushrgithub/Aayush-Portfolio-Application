ff472f7d64ede7c72813d04aa605929ba037e4e3
aayushrgithub <110468662+aayushrgithub@users.noreply.github.com>
2023-04-05 14:24:12 +0530

    Update property.js

--- pages/property.js
+++ pages/property.js
@@ -7,7 +7,7 @@ function property() {
     <div className='w-full'>
       <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
         <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
-        <img className='w-full absolute z-1 h-72 object-cover' src='https://i.stack.imgur.com/gNTbn.jpg' />
+        <img className='w-full absolute z-1 h-48 md:h-72 object-cover' src='https://i.stack.imgur.com/gNTbn.jpg' />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10'>
           <p className='py-2 text-4xl font-bold'>Amazon Clone</p>
           <p className='text-2xl'>ReactJS | Tailwind | Javascript</p>
@@ -30,7 +30,7 @@ function property() {
 
             Through this project, we'll discover a way to build a functional clone of Amazon's e-commerce website, relying on React and Firebase.</p>
           <button className='rounded-lg bg-gradient-to-r from-red-400 to-blue-400 text-gray-100 font-bold px-8 py-2 mt-4 mr-8'>Demo</button>
-          <button className='rounded-lg bg-gradient-to-r from-red-400 to-blue-400 text-gray-100 font-bold px-8 py-2 mt-4'>Code</button>
+        <a href='https://github.com/aayushrgithub/Aayush_Amazon'>  <button className='rounded-lg bg-gradient-to-r from-red-400 to-blue-400 text-gray-100 font-bold px-8 py-2 mt-4'>Code</button></a>
         </div>
         <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4 '>
           <div className='p-2'>
