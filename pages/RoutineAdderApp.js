5704532481d94365d37b19d0d91cfb2b760d29c7
aayushrgithub <110468662+aayushrgithub@users.noreply.github.com>
2023-04-05 14:22:48 +0530

    Update RoutineAdderApp.js

--- pages/RoutineAdderApp.js
+++ pages/RoutineAdderApp.js
@@ -7,7 +7,7 @@ function RoutineAdderApp() {
         <div className='w-full'>
             <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
                 <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
-                <img className='w-full absolute z-1 h-72 object-cover' src='https://2672686a4cf38e8c2458-2712e00ea34e3076747650c92426bbb5.ssl.cf1.rackcdn.com/2019-04-29-12-53-47.jpeg' />
+                <img className='w-full absolute z-1 h-48 md:h-72 object-cover' src='https://2672686a4cf38e8c2458-2712e00ea34e3076747650c92426bbb5.ssl.cf1.rackcdn.com/2019-04-29-12-53-47.jpeg' />
                 <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10'>
                     <p className='py-2 text-4xl font-bold'>Routine Adder Web App</p>
                     <p className='text-2xl'>ReactJS | Tailwind | Javascript</p>
@@ -25,7 +25,7 @@ function RoutineAdderApp() {
                         Go through your day's schedule, ignore the events you will not attend, pick a small number of tasks to work on, and block time for the most important ones.
                     </p>
                     <button className='rounded-lg bg-gradient-to-r from-red-400 to-blue-400 text-gray-100 font-bold px-8 py-2 mt-4 mr-8'>Demo</button>
-                    <button className='rounded-lg bg-gradient-to-r from-red-400 to-blue-400 text-gray-100 font-bold px-8 py-2 mt-4'>Code</button>
+                   <a href='https://github.com/aayushrgithub/RoutineAdder'> <button className='rounded-lg bg-gradient-to-r from-red-400 to-blue-400 text-gray-100 font-bold px-8 py-2 mt-4'>Code</button></a>
                 </div>
                 <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4 '>
                     <div className='p-2'>
